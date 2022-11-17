3c63644858b4ae7ee7f3964d7a6fb446903e34cc
Jake Speyer <jspeyer@globebiomedical.com>
2022-11-17 14:55:17 -0800

    Udpated files

---
+++ wayah_app/lib/widgets/indicator.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Indicator extends StatelessWidget {
+  const Indicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
